499909c3797c875598e62df5cc2d681bef0edda0
GraceAriane <tchoukeugrace@gmail.com>
2025-08-17 00:10:00 +0100

    navbar v1.1

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -7,19 +7,19 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 export function Header() {
   return (
     <header>
-        <div className="max-w-11/12 w-11/12 bg-amber-600 m-auto flex flex-row items-center justify-between h-16 p-4 mt-4">
+        <div className="max-w-11/12 w-11/12 bg-white m-auto flex flex-row items-center justify-between h-16.5 p-4 mt-4 rounded-2xl">
             <div className="flex items-center gap-0">
                 <FontAwesomeIcon icon={faBoltLightning} />
-                <h1>FastProfile</h1>
+                <h1 className="font-semibold">FastProfile</h1>
             </div>
             <ul className="hidden">
                 <li><a href="">A propos</a></li>
                 <li><a href="">Tarifs</a></li>
             </ul>
             <div className="flex items-center gap-3">
-                <a href="">Login</a>
-                <a href="">Sign up free</a>
-                <FontAwesomeIcon icon={faBars} />
+                <Button  variant="ghost" className="font-medium text-[#FACC15]"><a href="">Login</a></Button>
+                <Button className="font-normal bg-[#1E3A8A] text-white"><a href="">Sign up free</a></Button>
+                <FontAwesomeIcon icon={faBars} className="text-[#6B7280]" />
             </div>
         </div>
     </header>
