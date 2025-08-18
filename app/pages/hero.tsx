0aa6f68a1dc5d90bfc8f37d77563b5461050b5d8
GraceAriane <tchoukeugrace@gmail.com>
2025-08-18 12:03:16 +0100

    hero v1.1 responsive phone

--- app/pages/hero.tsx
+++ app/pages/hero.tsx
@@ -5,20 +5,20 @@ import alin from "../assets/images/alin_woong.svg"
 import mary from "../assets/images/marry_portman.svg"
 
 
-export function LandingPage(){
+export function Hero(){
     return(
-       <div className="flex flex-col items-center border-2 border-amber-950 border-solid ">
-            <div className="flex flex-col gap-10 border-2 border-amber-950 border-solid ">
+       <div className="flex flex-col items-center gap-[8vw]">
+            <div className="flex flex-col gap-10">
                 <div className="mt-28 flex flex-col gap-5">
-                    <h1 className="text-[9.6vw] leading-[107%] text-[#1E3A8A] font-black">Crée ton profil <span className="text-[#FACC15]">en ligne</span>  en un clin d’œil.</h1>
+                    <h1 className="text-[9.6vw] md:text-[6vw] leading-[107%] text-[#1E3A8A] font-black">Crée ton profil <span className="text-[#FACC15]">en ligne</span>  en un clin d’œil.</h1>
                     <p className="text-[#6B7280]">
                         Rassemble tous tes liens, projets et réseaux sociaux 
                         sur une page unique, élégante et rapide à partager.
                     </p>
                 </div>
                 <div className="flex flex-col gap-4">
-                    <Input placeholder="fastProfile.com/" className="h-19"/>
-                    <Input type="submit" value="Créer"/>
+                    <input placeholder="fastProfile.com/" className="h-19 placeholder:font-semibold border-2 border-solid border-[#3B559C] outline-none rounded-2xl px-3 focus:shadow-md focus:shadow-[#4867BE] focus:transition-shadow"/>
+                    <Button className="w-[200px] p-7 text-[5vw] bg-[#8F58E8] text-white hover:bg-[#8451D6] hover:transition-all">Créer</Button>
                 </div>
             </div>
             <div className="border-2 border-amber-950 border-solid">
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,7 +1,7 @@
 import { div, h1 } from "motion/react-client";
 import { Header } from "~/components/header";
 import { Footer } from "~/components/footer";
-import { LandingPage } from "./landing_page";
+import { Hero } from "./hero";
 
 export function Index(){
     return(
@@ -9,7 +9,7 @@ export function Index(){
             <div className="max-w-11/12 w-11/12 m-auto">
                 <Header/>
                 <main>
-                    <LandingPage></LandingPage>
+                    <Hero></Hero>
                 </main>
                 <Footer/>
             </div>
