c38af3226e996258f8c0fc6a6c09b1951ede8d51
GraceAriane <tchoukeugrace@gmail.com>
2025-08-18 13:17:33 +0100

    hero v1.2 responsive phone

--- app/pages/hero.tsx
+++ app/pages/hero.tsx
@@ -7,7 +7,7 @@ import mary from "../assets/images/marry_portman.svg"
 
 export function Hero(){
     return(
-       <div className="flex flex-col items-center gap-[8vw]">
+       <div className="flex flex-col items-center gap-[50px]">
             <div className="flex flex-col gap-10">
                 <div className="mt-28 flex flex-col gap-5">
                     <h1 className="text-[9.6vw] md:text-[6vw] leading-[107%] text-[#1E3A8A] font-black">Crée ton profil <span className="text-[#FACC15]">en ligne</span>  en un clin d’œil.</h1>
@@ -21,14 +21,14 @@ export function Hero(){
                     <Button className="w-[200px] p-7 text-[5vw] bg-[#8F58E8] text-white hover:bg-[#8451D6] hover:transition-all">Créer</Button>
                 </div>
             </div>
-            <div className="border-2 border-amber-950 border-solid">
-                <div className="">
+            <div className="flex">
+                <div className="perspective-distant rotate-[-5deg] animate-floatXg">
                     <img src={kameni} alt="" />
                 </div>
-                <div className="">
+                <div className="perspective-distant z-10 animate-floatY">
                     <img src={alin} alt="" />
                 </div>
-                <div className="">
+                <div className="perspective-distant rotate-[5deg] animate-floatXd">
                     <img src={mary} alt="" />
                 </div>
             </div>
