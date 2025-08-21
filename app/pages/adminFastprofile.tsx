2ca25c46c68b21a93494214ac57dd3cee373443d
GraceAriane <tchoukeugrace@gmail.com>
2025-08-21 17:43:38 +0100

    admin v1.2

--- app/pages/adminFastprofile.tsx
+++ app/pages/adminFastprofile.tsx
@@ -6,34 +6,34 @@ import { faArrowUpFromBracket, faGear, faChartLine,faUserGroup, faBars } from "@
 export function MyfastProfile(){
     return(
         <div>
-            <div>
-                <Avatar>
-                    <AvatarImage src="https://github.com/shadcn.png" />
-                    <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-                <div>
+            <div className="flex justify-between">
+                <div className="flex items-center justify-between w-[70%] ">
+                    <img src="https://github.com/shadcn.png" alt="" className="w-[60px] rounded-full"/>
                     <Button
                         variant="outline"
-                        className="font-medium text-[#1E3A8A] bg-white hover:transition-all"
+                        className="p-4 font-medium text-[#1E3A8A] bg-white hover:transition-all"
                     >
-                        <a href="">Login</a>
+                        <a href="">Try Pro for free</a>
                     </Button>
-                    <FontAwesomeIcon icon={faArrowUpFromBracket} />
-                    <FontAwesomeIcon icon={faGear} />
+                </div>
+                <div className="flex items-center gap-2 w-[30%] justify-center text-[#373737]">
+                    <FontAwesomeIcon icon={faArrowUpFromBracket} className="text-2xl"/>
+                    <FontAwesomeIcon icon={faGear} className="text-2xl"/>
                 </div>
             </div>
-            <div>
-                <h3>Tchoukeu Grace</h3>
+            <div className=" my-5 flex flex-col gap-2.5">
+                <h3 className="text-4xl font-bold text-[#373737]">tchoukeugrace</h3>
                 <p>fastprofile.com/tchoukeugrace</p>
             </div>
 
-            <div>
-                <Button>Pages</Button>
-                <Button>Archives</Button>
+            <div className="flex gap-5">
+                <Button className="rounded-2xl shadow-none py-4 px-6 text-[17px] bg-[#9C9C9C] text-white font-semibold">Pages</Button>
+                <Button className="rounded-2xl shadow-none py-4 px-6 text-[17px] bg-[#E8E8E8]">Archives</Button>
             </div>
 
             <div>
 
+                
             </div>
 
         </div>
--- app/routes/layout.tsx
+++ app/routes/layout.tsx
@@ -1,33 +1,37 @@
 import { Link, Outlet } from "react-router"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUpFromBracket, faGear, faChartLine,faUserGroup, faBars } from "@fortawesome/free-solid-svg-icons";
+import { Separator } from "~/components/ui/separator";
+import { MyfastProfile } from "~/pages/adminFastprofile";
 export default function Layout() {
   return (
-    <div className="flex flex-col bg-amber-500 h-[100vh]">
-        <div className="self-start bg-amber-950 h-[95%]">
-            <Outlet></Outlet>
+    <div className="flex flex-col h-[100vh] p-4">
+        <div className="self-start h-[95%] w-[100%]">
+            <MyfastProfile></MyfastProfile>
         </div>
-        <div className="self-end bg-blue-800 w-[100%] h-[10%] p-auto">
-            <div className="flex bg-amber-200 w-[95%] m-auto justify-between">
+        
+        <div className="self-end w-[100%] h-[10%] pt-[2.5%]">
+
+            <div className="flex w-[90%] m-auto justify-between text-[#6D6D6D]">
                 
-                <Link to="" className="flex flex-col justify-center items-center">
-                    <FontAwesomeIcon icon={faArrowUpFromBracket} />
-                    <p className="text-[0.625rem]">Mon FastProfile</p>
+                <Link to="" className="flex flex-col justify-center items-center gap-2">
+                    <FontAwesomeIcon icon={faArrowUpFromBracket} className="text-[25px]"/>
+                    <p className="text-[0.645rem]">FastProfile</p>
                 </Link>
                 
-                <Link to="" className="flex flex-col justify-center items-center">
-                    <FontAwesomeIcon icon={faChartLine} />
-                    <p className="text-[0.625rem]">Insights</p>
+                <Link to="" className="flex flex-col justify-center items-center gap-2">
+                    <FontAwesomeIcon icon={faChartLine} className="text-[25px]"/>
+                    <p className="text-[0.645rem]">Insights</p>
                 </Link>
 
-                <Link to="" className="flex flex-col justify-center items-center">
-                    <FontAwesomeIcon icon={faUserGroup} />
-                    <p className="text-[0.625rem]">Audience</p>
+                <Link to="" className="flex flex-col justify-center items-center gap-2">
+                    <FontAwesomeIcon icon={faUserGroup} className="text-[25px]"/>
+                    <p className="text-[0.645rem]">Audience</p>
                 </Link>
                 
-                <Link to="" className="flex flex-col justify-center items-center">
-                    <FontAwesomeIcon icon={faBars} />
-                    <p className="text-[0.625rem]">More</p>
+                <Link to="" className="flex flex-col justify-center items-center gap-2">
+                    <FontAwesomeIcon icon={faBars} className="text-[25px]"/>
+                    <p className="text-[0.645rem]">More</p>
                 </Link>
             </div>
 
