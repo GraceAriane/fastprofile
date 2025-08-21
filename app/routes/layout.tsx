abc6c161fd67e070ae9564218f161a8b40b4c468
GraceAriane <tchoukeugrace@gmail.com>
2025-08-21 23:41:12 +0100

    admin v1.4

--- app/routes/layout.tsx
+++ app/routes/layout.tsx
@@ -1,16 +1,38 @@
 import { Link, Outlet } from "react-router"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowUpFromBracket, faGear, faChartLine,faUserGroup, faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBoltLightning,faArrowUpFromBracket, faGear, faChartLine,faUserGroup, faBars } from "@fortawesome/free-solid-svg-icons";
 import { Separator } from "~/components/ui/separator";
 import { MyfastProfile } from "~/pages/adminFastprofile";
+import { Button } from "~/components/ui/button";
+
+
 export default function Layout() {
   return (
-    <div className="flex flex-col h-[100vh] p-4">
-        <div className="self-start h-[95%] w-[100%]">
-            <MyfastProfile></MyfastProfile>
+    <div className="flex flex-col h-[100vh] p-4 md:p-0 md:bg-[#1E3A8A]">
+        <div className="hidden md:flex md:items-center md:justify-between md:p-3 bg-[#1E3A8A]">
+            <FontAwesomeIcon icon={faBoltLightning} className="text-[#FACC15] text-2xl"/>
+            <Button
+            variant="outline"
+            className="font-medium text-white  hover:transition-all rounded-3xl"
+            >
+            <a href="">upgrade</a>
+            </Button>
+        </div>
+
+        <div className="self-start h-[95%] w-[100%] md:rounded-tl-3xl md:rounded-tr-3xl md:p-4  md:bg-[#F9FAFB]">
+
+            <div className="sidebar hidden md:flex bg-[#E1E3E8] w-[30%]">
+            dsfdsf
+            </div>
+            <div className="md:w-[70%]">
+                <MyfastProfile></MyfastProfile>
+            </div>
+
         </div>
         
-        <div className="self-end w-[100%] h-[10%] pt-[2.5%]">
+
+        
+        <div className="self-end w-[100%] h-[10%] pt-[2.5%] md:hidden">
 
             <div className="flex w-[90%] m-auto justify-between text-[#6D6D6D]">
                 
@@ -36,6 +58,8 @@ export default function Layout() {
             </div>
 
         </div>
+
+
     </div>
   )
 }
