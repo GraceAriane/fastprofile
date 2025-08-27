22787d8bad7096613b916f7494cfe55f177cc439
GraceAriane <tchoukeugrace@gmail.com>
2025-08-27 15:12:47 +0100

    design v1.6

--- app/pages/designProfile.tsx
+++ app/pages/designProfile.tsx
@@ -25,7 +25,55 @@ import { themes } from "~/config/themes";
 export function Design(){
     const [open,setOpen] = useState(false)
     const [theme, setTheme] = useState<keyof typeof themes>("light")
-    
+    const [content, setContent] = useState<"Profil" | "Thème" | "Fond" | "Style" | null>(null)
+
+    const handleOpen = (content: "Profil" | "Thème" | "Fond" | "Style") => {
+        setContent(content);
+        setOpen(true);
+    };
+    const contentDrawer =  (content: "Profil" | "Thème" | "Fond" | "Style" | null) => {
+        switch (content) {
+            case "Profil":
+                return(
+                  <div>
+
+                  </div>  
+                )
+                break;
+            case "Thème":
+                return(
+                    <div className="grid grid-cols-3 gap-4 md:grid-cols-[repeat(auto-fit,minmax(120px,1fr))] overflow-y-auto">
+                        <Button onClick={()=>setTheme("light")} variant="ghost" className="p-0 h-full w-full focus:border-2 rounded-2xl">
+                            <img src={light} alt="" className={`h-full w-full rounded-2xl`}/>
+                        </Button>
+                        <Button onClick={()=>setTheme("dark")} variant="ghost" className="p-0 h-full w-full focus:border-2 rounded-2xl">
+                            <img src={dark} alt="" className={`h-full w-full rounded-2xl`}/>
+                        </Button>
+                        <Button onClick={()=>setTheme("fancy")} variant="ghost" className="p-0 h-full w-full focus:border-2 rounded-2xl">
+                            <img src={fancy} alt="" className={`h-full w-full rounded-2xl`}/>
+                        </Button>
+                    </div>
+                )
+                break;
+            case "Fond":
+                return(
+                  <div>
+
+                  </div>  
+                )
+                break;
+            case "Style":
+                return(
+                  <div>
+
+                  </div>  
+                )
+                break;
+            default:
+                break;
+        }
+
+    };
 
     return(
         <div className="">
@@ -72,52 +120,43 @@ export function Design(){
                 </div>
 
                 <div className="fixed z-50 rounded-[24px] left-1/2 -translate-x-1/2 bottom-3.5 p-4 shadow-lg flex items-center justify-center bg-[#fff]">
-                    <Button variant="ghost" className="flex flex-col justify-center">
-                        <FontAwesomeIcon icon={faCircleUser} />
+                    <Button variant="ghost" className="flex flex-col justify-center" onClick={()=>handleOpen("Profil")}>
+                        <FontAwesomeIcon icon={faCircleUser}/>
                         Profil
                     </Button>
-                    <Button variant="ghost" className="flex flex-col justify-center" onClick={()=>setOpen(!open)}>
+                    <Button variant="ghost" className="flex flex-col justify-center" onClick={()=>handleOpen("Thème")}>
                         <FontAwesomeIcon icon={faSliders} />
                         Thème
                     </Button>
-                    <Button variant="ghost" className="flex flex-col justify-center">
+                    <Button variant="ghost" className="flex flex-col justify-center" onClick={()=>handleOpen("Fond")}>
                         <FontAwesomeIcon icon={faLayerGroup} />
                         Fond
                     </Button>
-                    <Button variant="ghost" className="flex flex-col justify-center">
+                    <Button variant="ghost" className="flex flex-col justify-center" onClick={()=>handleOpen("Style")}>
                         <FontAwesomeIcon icon={faPaintRoller} />
                         Style
                     </Button>
                 </div>
 
                 <div className={`grid grid-cols-1 grid-rows-[40px_40px_1fr] gap-2 h-[60%] p-4 absolute bottom-0 left-0 right-0 ${open ? "translate-y-0" : "translate-y-full"} z-1000 shadow-2xl rounded-tl-2xl rounded-tr-2xl bg-white transition-transform duration-500`}>
+
                     <div className="flex justify-between items-center">
-                        <h4 className="text-[18px]">Thème</h4>
+                        <h4 className="text-[18px]">{content}</h4>
                         <Button className="bg-[#cecece] rounded-full" onClick={()=>{setOpen(!open)}}>
                             <FontAwesomeIcon icon={faXmark} />
                         </Button>
                     </div>
-
-                        <Button className="text-xl" variant="ghost">Customisable</Button>
                         
-                    <div className="overflow-y-auto">
+                    <div className="pt-4">
                         
-                        <div className="grid grid-cols-3 gap-4 md:grid-cols-[repeat(auto-fit,minmax(120px,1fr))]">
-                            <Button onClick={()=>setTheme("light")} variant="ghost" className="p-0 h-full w-full focus:border-2 rounded-2xl">
-                                <img src={light} alt="" className={`h-full w-full rounded-2xl`}/>
-                            </Button>
-                            <Button onClick={()=>setTheme("dark")} variant="ghost" className="p-0 h-full w-full focus:border-2 rounded-2xl">
-                                <img src={dark} alt="" className={`h-full w-full rounded-2xl`}/>
-                            </Button>
-                            <Button onClick={()=>setTheme("fancy")} variant="ghost" className="p-0 h-full w-full focus:border-2 rounded-2xl">
-                                <img src={fancy} alt="" className={`h-full w-full rounded-2xl`}/>
-                            </Button>
-                        </div>
+                        {content && contentDrawer(content)}
 
                     </div>
+
                 </div>
 
             </div>
+
         </div>
     )
 }
