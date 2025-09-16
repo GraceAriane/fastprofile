0cc326dc00185c3ee31da5ed810bce868adff13c
GraceAriane <tchoukeugrace@gmail.com>
2025-09-16 10:19:34 +0100

    page v1.4

--- app/pages/hero.tsx
+++ app/pages/hero.tsx
@@ -18,7 +18,7 @@ export function Hero(){
                 </div>
                 <div className="flex flex-col gap-3 items-center md:items-start min-[1190px]:flex-row">
                     <input placeholder="fastProfile.com/" className="h-14 w-full  bg-[#fff] placeholder:font-semibold placeholder:text-[#969696] border-1 border-solid border-[#3B57A3] outline-none rounded-2xl px-3 focus:shadow-md focus:shadow-[#4867BE] focus:transition-shadow md:w-auto"/>
-                    <Link to="admin" className="self-center">
+                    <Link to="admin" className="self-center md:self-start xl:self-center">
                         <Button className="w-[200px] py-6 text-[4vw] bg-[#1E3A8A] text-white hover:bg-[#3B57A3] hover:transition-all md:text-[2.5vw] lg:text-[1.2rem]">Créer</Button>
                     </Link>
                 </div>
